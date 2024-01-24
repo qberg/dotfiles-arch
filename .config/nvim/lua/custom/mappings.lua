b4f8b37ad1e6c7372e6d618c7fd8572dfe9a807b
qberg <bs17b025@smail.iitm.ac.in>
2024-01-24 20:34:24 +0530

    final nvim config (for now atleast)

--- .config/nvim/lua/custom/mappings.lua
+++ .config/nvim/lua/custom/mappings.lua
@@ -1,7 +1,32 @@
 ---@type MappingsTable
 local M = {}
 
+function EscapePair()
+    local closers = {")", "]", "}", ">", "'", '"', "`", ","}
+    local line = vim.api.nvim_get_current_line()
+    local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+    local after = line:sub(col + 1, -1)
+    local closer_col = #after + 1
+    local closer_i = nil
+    for i, closer in ipairs(closers) do
+        local cur_index, _ = after:find(closer)
+        if cur_index and (cur_index < closer_col) then
+            closer_col = cur_index            
+            closer_i = i
+        end    
+    end    
+    if closer_i then
+        vim.api.nvim_win_set_cursor(0, {row, col + closer_col})
+    else
+        vim.api.nvim_win_set_cursor(0, {row, col + 1})
+    end
+end
+
 M.general = {
+  i = {
+    ["<C-p>"] = {"<cmd>lua EscapePair()<CR>", opts= { noremap = true, silent = true }},
+  },
+
   n = {
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
 
